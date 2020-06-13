d4d5b5ba0942eb7c06ae9b2d9d57f7a700de8996
allen52melody <66862398+allen52melody@users.noreply.github.com>
2020-06-13 15:27:15 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,50 @@
-from flask import Flask
-from flask import render_template
-app=Flask(__name__)
+# -*- coding: utf-8 -*-
+from flask import Flask, request, abort
 
-@app.route("/template")
-def aaa():
-    return render_template('index.html')
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
 
-if __name__=='__main__':
+app = Flask(__name__)
+
+line_bot_api = LineBotApi('WCy+FCGEj7wycHwDAhpOX4qv8vOo3S/snjEczV2wp/i2MAk5AWpT6zQgxgdcC+K27ChJ2c0Bn9UDGI+gHmU3OdvPZXJaNs5eEW4sW9YBgdKMPrC5bajOfuTGrQ6CtkPNnLKA+h+fWN1CDA6fi4F+SQdB04t89/1O/w1cDnyilFU=')
+# Channel access token (long-lived)
+handler = WebhookHandler('2cc552e567cabbfd6835618539510d82')
+# Channel secret 
+
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+ 
+
+if __name__ == '__main__':
     app.run(debug=True)

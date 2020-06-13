4c257467eb8bca58702b0c10fdae78e301373d77
allen52melody <66862398+allen52melody@users.noreply.github.com>
2020-06-13 16:20:17 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -7,22 +7,18 @@ from linebot import (
 from linebot.exceptions import (
     InvalidSignatureError
 )
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
+from linebot.models import *
 
 app = Flask(__name__)
 
+# Channel access token (long-lived)
 line_bot_api = LineBotApi('WCy+FCGEj7wycHwDAhpOX4qv8vOo3S/snjEczV2wp/i2MAk5AWpT6zQgxgdcC+K27ChJ2c0Bn9UDGI+gHmU3OdvPZXJaNs5eEW4sW9YBgdKMPrC5bajOfuTGrQ6CtkPNnLKA+h+fWN1CDA6fi4F+SQdB04t89/1O/w1cDnyilFU=')
 # Channel access token (long-lived)
 handler = WebhookHandler('2cc552e567cabbfd6835618539510d82')
-# Channel secret 
-
 
 
 @app.route("/callback", methods=['POST'])
 def callback():
-
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
@@ -38,13 +34,51 @@ def callback():
 
     return 'OK'
 
+def getNews():
+    """
+    建立一個抓最新消息的function
+    """
+    import requests
+    import re
+    from bs4 import BeautifulSoup
+
+    url = 'https://www.ettoday.net/news/focus/3C%E5%AE%B6%E9%9B%BB/'
+    r = requests.get(url)
+    reponse = r.text
+
+    url_list = re.findall(r'<h3><a href="/news/[\d]*/[\d]*.htm" .*>.*</a>',reponse)
+
+    soup = BeautifulSoup(url_list[0])
+    url = 'https://fashion.ettoday.net/' + soup.find('a')['href']
+    title = soup.text
+
+
+    tmp = title + ': ' +url
+    return tmp
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
- 
+    # 傳送文字
+    if event.message.text == '傳送文字':
+        message = TextSendMessage(getNews())
+          # 傳送圖片
+    elif event.message.text == '傳送圖片':
+        message = ImageSendMessage(
+            original_content_url='https://i.imgur.com/QPJ8A1b.png',
+            preview_image_url='https://i.imgur.com/QPJ8A1b.png'
+        )
+
+
+    # 傳送貼圖
+    elif event.message.text == '傳送貼圖':
+        message = StickerSendMessage(
+            package_id='1',
+            sticker_id='1'
+        )
+    else:
+        message = TextSendMessage(text=event.message.text)
+    line_bot_api.reply_message(event.reply_token, message)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
